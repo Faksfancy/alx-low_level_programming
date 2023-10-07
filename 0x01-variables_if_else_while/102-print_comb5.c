f2b73b991e6be465c8ea0ae25eab0bd929a87f7a
Faksfancy <ayfakolujo@gmail.com>
2023-10-06 19:34:33 -0700

    102-print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,9 +12,9 @@ int main(void)
 {
 	int i, j;
 
-	for (int i = 0; i <= 99; i++)
+	for (i = 0; i <= 99; i++)
 	{
-		for (int j = i; j <= 99; j++)
+		for (j = i; j <= 99; j++)
 		{
 			if (i != j)
 			{
