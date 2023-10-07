ee0ab0629c17382771d5eaa79a6ce42fe098ed11
Faksfancy <ayfakolujo@gmail.com>
2023-10-06 18:32:05 -0700

    101-print_comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,11 +10,13 @@
 
 int main(void)
 {
-	for (int i = 0; i < 10; i++)
+	int i, j, k;
+
+	for (i = 0; i < 10; i++)
 	{
-		for (int j = i + 1; j < 10; j++)
+		for (j = i + 1; j < 10; j++)
 		{
-			for (int k = j + 1; k < 10; k++)
+			for (k = j + 1; k < 10; k++)
 			{
 				putchar('0' + i);
 
