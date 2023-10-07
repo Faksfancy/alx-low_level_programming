a6c3200adf506189cba8845d55ae84b9f280a826
Faksfancy <ayfakolujo@gmail.com>
2023-10-06 18:57:07 -0700

    102-print_comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - entry function for program
+ *
+ * Description: print '00', '01'..'99'
+ *
+ * Return: always 0 (success)
+*/
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = i; j < 100; j++)
+		{
+			putchar('0' + (i / 10));
+
+			putchar('0' + (i % 10));
+
+			putchar(' ');
+
+			putchar('0' + (j / 10));
+
+			putchar('0' + (j % 10));
+
+			if (i != 99 || j != 99)
+			{
+				putchar(',');
+
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
