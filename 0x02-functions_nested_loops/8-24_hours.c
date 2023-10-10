3f9e14063f5bc4061305206cca0d343030f02622
Faksfancy <ayfakolujo@gmail.com>
2023-10-10 15:35:11 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * jack_bauer - check the code
+ *
+ * Return: Always 0.
+*/
+
+void jack_bauer(void)
+{
+	int hT, hO, mT, mO;
+
+	hT = 0;
+	while (hT <= 2)
+	{
+		hO = 0;
+		while (hO <= 9)
+		{
+			if (hT == 2 && hO > 3)
+			{
+				break;
+			}
+			mT = 0;
+			while (mT <= 5)
+			{
+				mO = 0;
+				while (mO <= 9)
+				{
+					_putchar(ht + '0');
+
+					_putchar(hO + '0');
+
+					_putchar(':');
+
+					_putchar(mT + '0');
+
+					_putchar(mO + '0');
+
+					mO++;
+				}
+				mT++;
+			}
+			hO++;
+		}
+		hT++;
+	}
+}
