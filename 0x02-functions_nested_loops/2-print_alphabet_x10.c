b61a6f9fe6a8d33476ca890fb405c8ee79fcf5a1
Faksfancy <ayfakolujo@gmail.com>
2023-10-10 09:30:21 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,18 +8,18 @@
 
 void print_alphabet_x10(void)
 {
-	int i = 97, j;
+	int i = 97, j = 0;
 
-	while (i < 123)
+	while (j < 10)
 	{
-		j = 0;
-		while (j < 10)
+		
+		while (i < 123)
 		{
 			_putchar(i);
-			j++;
+			i++;
 		}
 		_putchar('\n');
-		i++;
+		j++;
 	}
 
 }
