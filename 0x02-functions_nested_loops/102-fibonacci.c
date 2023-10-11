73bf09d884e5032da22fccfe48fc9b9707b48862
Faksfancy <ayfakolujo@gmail.com>
2023-10-11 13:36:53 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,22 +10,26 @@ int main(void)
 {
 	int n = 50;
 
-	long long int fib[n];
+	int i;
+
+	int fib[n];
 
 	fib[0] = 1;
 	fib[1] = 2;
 
-	for (int i = 2; i < n; i++)
+	for (i = 2; i < n; i++)
 	{
 		fib[i] = fib[i - 1] + fib[i - 2];
 	}
 
-	for (int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
-		printf("%lld", fib[i]);
+		printf("%d", fib[i]);
 		if (i < n - 1)
 		{
 			printf(", ");
 		}
 	}
+	printf("\n");
+	return (0);
 }
