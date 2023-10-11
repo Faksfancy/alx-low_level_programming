5997f3a84140ed567e441a85bae5536333469e12
Faksfancy <ayfakolujo@gmail.com>
2023-10-10 18:50:00 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,34 +8,63 @@
 
 void print_times_table(int n)
 {
-	int i,j;
+	int i, j;
 
 	if (n < 0 || n > 15)
 	{
 		return;
 	}
+	int max_result = n * n;
+	int max_digits = 0;
+	int temp_max = max_result;
 
-	for (i = 0; i <= n; i++)
+	while (temp_max > 0)
 	{
-		for (j = 0; j <= n; j++)
+		max_digits++;
+		temp_max /= 10;
+	}
+
+	for (int i = 0; i <= n; i++)
+	{
+		for (int j = 0; j <= n; j++)
 		{
 			int result = i * j;
 
+			int digits = 0;
+
+			int temp_result = result;
+
+			while (temp_result > 0)
+			{
+				digits++;
+				temp_result /= 10;
+			}
+			int spaces = max_digits - digits;
+
+			for (int s = 0; s < spaces; s++)
+			{
+				putchar(' ');
+			}
+
 			if (j > 0)
 			{
-				_putchar(' ');
+				putchar(',');
+				putchar(' ');
 			}
 
 			if (result < 10)
 			{
-				_putchar(result + '0');
+				putchar('0' + result);
 			}
 			else
 			{
-				_putchar(result / 10 + '0');
-				_putchar(result % 10 + '0');
+				while (result > 0)
+				{
+					putchar('0' + result % 10);
+					result /= 10;
+				}
 			}
 		}
-		_putchar('\n');
+		putchar('\n');
 	}
 }
