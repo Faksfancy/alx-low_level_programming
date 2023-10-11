f6118ba23627b14fec63a35ef22555a17790f69e
Faksfancy <ayfakolujo@gmail.com>
2023-10-11 15:45:51 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,9 +9,7 @@
 
 void print_times_table(int n)
 {
-	int i, j;
-
-	int result;
+	int i, j, result;
 
 	if (n < 0 || n > 15)
 	{
@@ -24,13 +22,38 @@ void print_times_table(int n)
 			for (j = 0; j <= n; j++)
 			{
 				result = i * j;
-				printf("%d", result);
-				if (j < n)
+				if (result < 10)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
+					_putchar(result + '0');
+					if (j < n)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+					}
+				}
+				else if (result < 100)
+				{
+					_putchar((result / 10) + '0');
+					_putchar((result % 10) + '0');
+					if (j < n)
+					{
+						_putchar(',');
+						_putchar(' ');
+						_putchar(' ');
+					}
+				}
+				else
+				{
+					_putchar((result / 100) + '0');
+					_putchar(((result / 10) % 10) + '0');
+					_putchar((result % 10) + '0');
+					if (j < n)
+					{
+						_putchar(',');
+						_putchar(' ');
+					}
 				}
 			}
 			_putchar('\n');
