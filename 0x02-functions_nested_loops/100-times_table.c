52c34784d605ac92a1e5e3a1b1d728b9a14d3f38
Faksfancy <ayfakolujo@gmail.com>
2023-10-11 16:34:27 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_times_table - check the code.
@@ -19,42 +18,31 @@ void print_times_table(int n)
 	{
 		for (i = 0; i <= n; i++)
 		{
+			_putchar('0');
 			for (j = 0; j <= n; j++)
 			{
+				_putchar(',');
+				_putchar(' ');
+
 				result = i * j;
-				if (result < 10)
+				if (result <= 99)
 				{
-					_putchar(result + '0');
-					if (j < n)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(' ');
-					}
+					_putchar(' ');
 				}
-				else if (result < 100 && result >= 10)
+				if (result <= 9)
 				{
-					_putchar((result / 10) + '0');
-					_putchar((result % 10) + '0');
-					if (j < n)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-					}
+					_putchar(' ');
 				}
-				else
+				if (result >= 100)
 				{
 					_putchar((result / 100) + '0');
 					_putchar(((result / 10) % 10) + '0');
-					_putchar((result % 10) + '0');
-					if (j < n)
-					{
-						_putchar(',');
-						_putchar(' ');
-					}
 				}
+				else if (result <= 99 && result >= 10)
+				{
+					_putchar((result / 10) + '0');
+				}
+				_putchar((result % 10) + '0');
 			}
 			_putchar('\n');
 		}
