dd8475d2d96ce665824e5d3f65360937aea51fde
Faksfancy <ayfakolujo@gmail.com>
2023-10-12 11:09:24 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - check the code
+ * Return: Always 0.
+*/
+
+void print_numbers(void)
+{
+	int n;
+
+	if (n >= '48' && n <= '57')
+	{
+		_putchar(n);
+		_putchar('\n');
+	}
+	else
+	{
+		return (0);
+	}
+}
